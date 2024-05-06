5cc5b6c7ea96731d5033f62c10ed9331f5317c15
Omar Hidalgo <omar_ehr@outlook.com>
2024-05-06 01:41:39 +0000

    FIX use of conventions  IN  refacotr

--- refactor.js
+++ refactor.js
@@ -3,44 +3,44 @@ let inventory = [
     name: "apples",
     id: 1,
     quantity: 20,
-    price: 0.85,
+    price: 0.85
   },
   {
     name: "bananas",
-    quantity: 10,
     id: 2,
-    price: 1.25,
+    quantity: 10,
+    price: 1.25
   },
   {
     name: "cherries",
-    quantity: 5,
-    price: 2.5,
     id: 3,
+    quantity: 5,
+    price: 2.5
   },
   {
     name: "dates",
-    quantity: 2,
-    price: 3.0,
     id: 4,
+    quantity: 2,
+    price: 3.0
   },
   {
     name: "elderberries",
+    id: 5,
     quantity: 1,
     price: 4.0,
-    id: 5,
   },
   {
     name: "Watermelon",
+    id: 6,
     quantity: 0,
     price: 5.0,
-    id: 6,
   },
   {
     name: "grapes",
-    quantity: 0,
-    price: 6.0,
     id: 7,
-  },
+    quantity: 0,
+    price: 6.0
+  }
 ];
 
 let order = {
@@ -65,21 +65,21 @@ function processOrder(order, inventory) {
   let total = 0;
   let shipping = 0;
   let tax = 0;
-  let grand_total = 0;
+  let grandTotal = 0;
   let items = order.items;
   let country = order.country;
-  let total_items_ = 0;
-  let total_items_Price = 0;
-  let total_items_Tax = 0;
-  let total_items_Shipping = 0;
-  let total_items_GrandTotal = 0;
+  let totalItems = 0;
+  let totalItemsPrice = 0;
+  let totalItemsTax = 0;
+  let totalItemsShipping = 0;
+  let totalItemsGrandTotal = 0;
 
   for (let i = 0; i < items.length; i++) {
     // Get the item details
     let item = items[i];
     let itemID = item.id;
-    let item_quantity = item.quantity;
-    let item_price = 0;
+    let itemQuantity = item.quantity;
+    let itemPrice = 0;
     let itemTax = 0;
     let itemShipping = 0;
     let itemGrandTotal = 0;
@@ -87,43 +87,40 @@ function processOrder(order, inventory) {
     for (let j = 0; j < inventory.length; j++) {
       // Find the item in the inventory
       if (inventory[j].id === itemID) {
-        item_price = inventory[j].price;
-        itemTax = item_price * 0.1;
-        itemShipping = item_price * 0.05;
-        itemGrandTotal = item_price + itemTax + itemShipping;
-        total_items_Price += item_price * item_quantity;
-        total_items_Tax += itemTax * item_quantity;
-        total_items_Shipping += itemShipping * item_quantity;
-        total_items_GrandTotal += itemGrandTotal * item_quantity;
-        total_items_ += item_quantity;
+        itemPrice = inventory[j].price;
+        itemTax = itemPrice * 0.1;
+        itemShipping = itemPrice * 0.05;
+        itemGrandTotal = itemPrice + itemTax + itemShipping;
+        totalItemsPrice += itemPrice * itemQuantity;
+        totalItemsTax += itemTax * itemQuantity;
+        totalItemsShipping += itemShipping * itemQuantity;
+        totalItemsGrandTotal += itemGrandTotal * itemQuantity;
+        totalItems += itemQuantity;
         break;
       }
     }
   }
 
-  if (country === "United States") {
-    shipping = 5;
-  } else if (country === "Canada") {
-    shipping = 10;
-  } else {
-    shipping = 15;
-  }
+  if (country === "United States") shipping = 5;
+  else if (country === "Canada")  shipping = 10;
+  else shipping = 15;
 
-  tax = total_items_Tax;
-  total = total_items_Price;
-  grand_total = total + tax + shipping;
+  tax = totalItemsTax;
+  total = totalItemsPrice;
+  grandTotal = total + tax + shipping;
 
   return {
     total,
     shipping,
     tax,
-    grand_total,
-    total_items_,
-    total_items_Price,
-    total_items_Tax,
-    total_items_Shipping,
-    total_items_GrandTotal,
+    grandTotal,
+    totalItems,
+    totalItemsPrice,
+    totalItemsTax,
+    totalItemsShipping,
+    totalItemsGrandTotal,
   };
 }
 
+processOrder();
 console.log(processOrder(order, inventory));
